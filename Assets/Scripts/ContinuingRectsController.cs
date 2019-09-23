b756fad2c985ec2ec76537a6ef686d00907f4a6a
Koba <koba@dummy.com>
2019-09-23 16:09:29 +0900

    Prefab, script追加

---
+++ Assets/Scripts/ContinuingRectsController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ContinuingRectsController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject go = GameObject.CreatePrimitive(PrimitiveType.Plane);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
